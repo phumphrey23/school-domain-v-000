a2b351a4713153664a5b4713384b9361b9f150f8
Precious Humphrey <phumphrey23@gmail.com>
2018-05-20 17:30:56 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+  
+  def roster
+    @roster
+  end
+
+  def grade
+    @roster.detect do |x, y|
+      if x == level
+        return y
+    end
+   end
+  end
+
+  def sort
+    @roster.each do |key,value|
+      value.sort!
+    end
+  end
+  
+end
